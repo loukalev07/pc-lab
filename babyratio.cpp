ccadead268b010ca61a6a54266f59af69adaa584
loukalev07 <loukalev07@gmail.com>
2026-03-02 12:17:56 +0200

    Add files via upload

---
+++ babyratio.cpp
@@ -0,0 +1,52 @@
+#ifndef CONTEST
+#include "babyratio.hpp"
+#include <iostream>
+#endif
+
+using namespace std;
+
+int rational::gcd (int a, int b) {
+    if (b == 0) return a;
+    return gcd (b, a % b);
+}
+
+rational::rational (int n, int d) {
+    nom = n;
+    den = d;
+}
+
+rational rational::add (rational r) {
+    int n = nom * r.den + r.nom * den;
+    int d = den * r.den;
+    n /= gcd (n, d);
+    d /= gcd (n, d);
+    return rational (n, d);
+}
+
+rational rational::sub (rational r) {
+    int n = nom * r.den - r.nom * den;
+    int d = den * r.den;
+    n /= gcd (n, d);
+    d /= gcd (n, d);
+    return rational (n, d);
+}   
+
+rational rational::mul (rational r) {
+    int n = nom * r.nom;
+    int d = den * r.den;
+    n /= gcd (n, d);
+    d /= gcd (n, d);
+    return rational (n, d);
+}
+
+rational rational::div (rational r) {
+    int n = nom * r.den;
+    int d = den * r.nom;
+    n /= gcd (n, d);
+    d /= gcd (n, d);
+    return rational (n, d);
+}
+
+void rational::print () {
+    cout << nom << "/" << den;
+}
