c4b86f3613b53db99f0af6c8b287b5757619c5cb
loukalev07 <loukalev07@gmail.com>
2026-03-06 10:09:16 +0200

    Simplify rational class constructor and methods
    Refactor rational class methods to simplify fraction handling and remove unnecessary GCD calculations.

--- babyratio.cpp
+++ babyratio.cpp
@@ -13,49 +13,45 @@ int rational::gcd (int a, int b) {
 }
 
 rational::rational (int n, int d) {
+    int Gcd = gcd(abs(n), abs(d));
+    n = n / Gcd;
+    d = d / Gcd;
+
     if (n < 0 && d < 0) {
-        nom = -n / gcd (-n, -d);
-        den = -d / gcd (-n, -d);
+        nom = -n;
+        den = -d;
     }
     else if (d<0 && n>=0) {
-        nom = -n / gcd (n,-d);
-        den = -d / gcd (n,-d);
+        nom = -n;
+        den = -d;
     }
     else {
-        nom = n / gcd (abs(n), d);
-        den = d / gcd (abs(n), d);
+        nom = n;
+        den = d;
     }
 }
 
 rational rational::add (rational r) {
     nom = nom * r.den + r.nom * den;
     den = den * r.den;
-    int n = nom / gcd (abs(nom), den);
-    int d = den / gcd (abs(nom), den);
     return rational (n, d);
 }
 
 rational rational::sub (rational r) {
-    nom = nom * r.den - r.nom * den;
-    den = den * r.den;
-    int n = nom / gcd (abs(nom), den);
-    int d = den / gcd (abs(nom), den);
+    int n = nom * r.den - r.nom * den;
+    int d = den * r.den;
     return rational (n, d);
 }   
 
 rational rational::mul (rational r) {
-    nom = nom * r.nom;
-    den = den * r.den;
-    int n = nom / gcd (abs(nom), den);
-    int d = den / gcd (abs(nom), den);
+    int n = nom * r.nom;
+    int d = den * r.den;
     return rational (n, d);
 }
 
 rational rational::div (rational r) {
-    nom = nom * r.den;
-    den = den * r.nom;
-    int n = nom / gcd (abs(nom), den);
-    int d = den / gcd (abs(nom), den);
+    int n = nom * r.den;
+    int d = den * r.nom;
     return rational (n, d);
 }
 
