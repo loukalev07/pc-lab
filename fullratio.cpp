5fb0fc0cfbc497ff4a49ef6468f77ad4d2f8dbf8
loukalev07 <loukalev07@gmail.com>
2026-03-06 10:22:51 +0200

    Simplify rational constructor and operators

--- fullratio.cpp
+++ fullratio.cpp
@@ -11,32 +11,46 @@ int rational::gcd(int a, int b) {
 }
 
 rational::rational (int n, int d) {
-    nom = n;
-    den = d;
+    int Gcd = gcd(abs(n), abs(d));
+    n = n / Gcd;
+    d = d / Gcd;
+
+    if (n < 0 && d < 0) {
+        nom = -n;
+        den = -d;
+    }
+    else if (d<0 && n>=0) {
+        nom = -n;
+        den = -d;
+    }
+    else {
+        nom = n;
+        den = d;
+    }
 }
 
 rational operator + (const rational &x, const rational &y) {
     int n = x.nom * y.den + y.nom * x.den;
     int d = x.den * y.den;
-    return rational((n/rational::gcd(n,d)), (d/rational::gcd(n,d)));
+    return rational(n, d);
 }
 
 rational operator - (const rational &x, const rational &y) {
     int n = x.nom * y.den - y.nom * x.den;
     int d = x.den * y.den;
-    return rational((n/rational::gcd(n,d)), (d/rational::gcd(n,d)));
+    return rational(n,d);
 }
 
 rational operator * (const rational &x, const rational &y) {
     int n = x.nom * y.nom;
     int d = x.den * y.den;
-    return rational((n/rational::gcd(n,d)), (d/rational::gcd(n,d)));
+    return rational(n,d);
 }
 
 rational operator / (const rational &x, const rational &y) {
     int n = x.nom * y.den;
     int d = x.den * y.nom;
-    return rational((n/rational::gcd(n,d)), (d/rational::gcd(n,d)));
+    return rational(n,d);
 }
 
 ostream & operator << (ostream &out, const rational &x) {
